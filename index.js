558fd22bf9036853f6d93d71f5e3c9a803fb9a9a
Steve <kamaustevek78@gmail.com>
2023-04-11 10:26:48 +0300

    Done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let apikey = 'En1FPwAxA9aLYNEJx6SOu0GAcxn3PrES'
+let apikey = "En1FPwAxA9aLYNEJx6SOu0GAcxn3PrES"
 let queryURLBase="https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";//election&api-key="En1FPwAxA9aLYNEJx6SOu0GAcxn3PrES";
 //
 let searchTerm="";
@@ -10,11 +10,12 @@ let searchObject="";
 
 
 $(document).ready(function(){
+    // alert("i am alive");
     
-    // add event handler to runSearch button
-    $("#runSearch").on("click", searchArticles);
-    $("#clearAll").on("click", clearArticles);
-
+    //add event handler to runSearch button
+    $("#runSearch").on("click",searchArticles);
+    $("#clearAll").on("click",clearArticles);
+    
 });
 
 function clearArticles(){
@@ -32,19 +33,19 @@ function searchArticles(event){
         method:'GET'
     }).then(function(data){
         // console.log(data);
-        // searchObject = data;
+        // searchObject=data;
         renderResults(data);
     });
-
+    
 }
 
 function renderResults(data) {
     // console.log(data);
 
-    let searchCount = $("#numRecordsSelect").val();
-    // clear old divs
+    let  searchCount = $("#numRecordsSelect").val();
+    //clear old divs
     $('#search-results').empty();
-    data.response.docs = data.response.docs.slice(1, parseInt(searchCount)+1);
+    data.response.docs=data.response.docs.slice(1,parseInt(searchCount)+1);
     data.response.docs.forEach(function(element) {
         const articleDiv = $('<div>');
         articleDiv.text(element.abstract);
@@ -53,35 +54,41 @@ function renderResults(data) {
     });
 }
 
-function buildURL() {
-    searchTerm = $("#searchTerm").val().trim();
-    searchCount = $("numRecordsSelect").val();
-    startYear = $("#startYear").val().trim();
+function buildURL(){
+    searchTerm=$("#searchTerm").val().trim();
+    searchCount = $("#numRecordsSelect").val();
+    startYear=$("#startYear").val().trim();
+    endYear=$("#endYear").val().trim();
 
-    let returnURL = `${queryURLBase}${searchTerm}`;
+    let returnUrl=`${queryURLBase}${searchTerm}`;
 
     //user provides no values
-    if(startYear === "" && endYear === ""){
-        returnURL = `${queryURLBase}${searchTerm}&api-key=${En1FPwAxA9aLYNEJx6SOu0GAcxn3PrES}`;
-        return returnURL;
+    if(startYear === "" &&  endYear === ""){
+        returnUrl=`${queryURLBase}${searchTerm}&api-key=${apikey}`;
+        return returnUrl;
 
-
+        
     }
-    // user provides end year only
+    //user provides end year only
     if( startYear === "" && endYear !== "")
     {
-        returnURL = `${queryURLBase}${searchTerm}&facet=true&begin_date=${startYear}&api-key=${En1FPwAxA9aLYNEJx6SOu0GAcxn3PrES}`;
-        return returnURL;
-
-
+        returnUrl=`${queryURLBase}${searchTerm}&facet=true&end_date=${endYear}&api-key=${apikey}`;
+        return returnUrl;
+        
     }
-    // user provides both values
+    //user provide start year only
+    if( startYear !== "" && endYear === "")
+    {
+        returnUrl=`${queryURLBase}${searchTerm}&facet=true&begin_date=${startYear}&api-key=${apikey}`;
+        return returnUrl;
+        
+    }
+    //user provides both values
     if( startYear !== "" && endYear !== "")
     {
-    returnURL = `${queryURLBase}${searchTerm}&facet=true&begin_date=${startYear}&end_date=${endYear}&api-key=${En1FPwAxA9aLYNEJx6SOu0GAcxn3PrES}`;
-    return returnURL;
-    
+        returnUrl=`${queryURLBase}${searchTerm}&facet=true&begin_date=${startYear}&end_date=${endYear}&api-key=${apikey}`;
+        return returnUrl;
+        
     }
-
-    
+   
 }
